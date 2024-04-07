ba4433cfe8c36b718a04000d11b0ab7c670ec832
gaguriee <74501631+gaguriee@users.noreply.github.com>
2024-04-07 19:58:07 +0900

    feat : redis 연결 및 refresh token filter 추가

---
+++ src/main/java/com/seoultech/capstone/config/RedisConfig.java
@@ -0,0 +1,44 @@
+package com.seoultech.capstone.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.port}")
+    private int redisPort;
+
+    @Value("${redis.index.token}")
+    private int redisTokenIndex;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+
+        redisStandaloneConfiguration.setDatabase(redisTokenIndex);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        return redisTemplate;
+    }
+
+
+}
---
+++ src/main/java/com/seoultech/capstone/domain/auth/jwt/RefreshFilter.java
@@ -0,0 +1,51 @@
+package com.seoultech.capstone.domain.auth.jwt;
+
+import com.seoultech.capstone.exception.CustomException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static com.seoultech.capstone.exception.ErrorCode.INVALID_TOKEN;
+
+@Slf4j
+@RequiredArgsConstructor
+public class RefreshFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+    private final RedisTemplate<String, String> redisTemplate;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+
+
+        if (request.getRequestURI().equals("/auth/tokenRefresh")) {
+
+            String jwt = tokenProvider.resolveToken(request);
+
+            if (jwt != null) {
+                tokenProvider.validateToken(jwt);
+
+                Authentication authentication = tokenProvider.resolveToken(jwt);
+                String refreshToken = redisTemplate.opsForValue().get(authentication.getName());
+                if (refreshToken == null) {
+                    throw new CustomException(INVALID_TOKEN, "Refresh Token not found.");
+                } else if (!refreshToken.equals(jwt)) {
+                    throw new CustomException(INVALID_TOKEN, "Refresh Token doesn't match.");
+                }            }
+
+        }
+
+        chain.doFilter(request, response);
+
+    }
+
+}
